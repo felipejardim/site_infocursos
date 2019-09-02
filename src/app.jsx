5d4d77a9901554508f3d14aaf0822500c37f652d
felipejardim <felipejardim95@gmail.com>
2019-09-02 16:13:43 -0400

    tentando criar um async-select

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
-import Select from 'react-select'
+import AsyncSelect from 'react-select/async'
+
 
 
 const jason = require('../src/json/cursos.json')
 
-//código para extrair as opções para o select
-let cursos = jason.map(obj=>obj.Curso)
-cursos = cursos.filter(function(elem, pos, self) {
+//código para extrair as opções para o select (old)
+let cursos = jason.map(obj=>obj.Curso).filter(function(elem, pos, self) {
     return self.indexOf(elem) === pos;
 })
 
@@ -26,11 +26,45 @@ anos = anos.filter(function(elem, pos, self) {
 })
 
 window.dadoss = [campi,cursos,turnos,anos]
+window.jason = jason
 
+let options = async(requerido, dados)=>{
+    let filtro = jason
 
-let options = function(item){
-    let arr = item.map((key)=>{return {value:key, label:key}})
-    return arr 
+    if(requerido!=='campus' && dados.campus!==''){
+        filtro = filtro.filter(item=>item.Campus===dados.campus?item:null);
+    }
+
+    if(requerido!=='curso' && dados.curso!==''){
+        filtro = filtro.filter(item=>item.Curso===dados.curso?item:null);
+    }
+
+    if(requerido!=='turno' && dados.turno!==''){
+        filtro = filtro.filter(item=>item.Turno===dados.turno?item:null);
+    }
+
+    if(requerido!=='ano' && dados.ano!==''){
+        filtro = filtro.filter(item=>item.Ano===dados.ano?item:null);
+    }
+
+    console.log(filtro)
+
+    switch (requerido) {
+        case 'campus':
+            return filtro.map(obj=>obj.Campus).filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+        case 'curso':
+            return filtro.map(obj=>obj.Curso).filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+        case 'turno':
+            return filtro.map(obj=>obj.Turno).filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+        case 'ano':
+            return filtro.map(obj=>obj.Ano).filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+        default:
+            break;
+    }
+
+    //metodo antigo, apenas pega o valor de cada item e não filtra
+    //let arr = item.map((key)=>{return {value:key, label:key}})
+    //return arr 
 }
 
 export default function App(){
@@ -39,16 +73,16 @@ export default function App(){
 
     return <>
         <label className="legenda">Campus:</label>
-        <Select options={options(campi) } onChange={(e)=>{setDados(Object.assign(dados,{campus: e.value}))}} placeholder="" className="select"/>
+        <AsyncSelect  /*defaultOptions={options('campus', dados)}*/ loadOptions={options('campus', dados)} onChange={(e)=>{setDados(Object.assign(dados,{campus: e.value}))}} placeholder="" className="select"/>
         
         <label className="legenda">Curso:</label>
-        <Select options={options(cursos)} onChange={(e)=>{setDados(Object.assign(dados,{curso: e.value}))}} placeholder="" className="select"/>
+        <AsyncSelect defaultOptions={options('curso', dados)} onChange={(e)=>{setDados(Object.assign(dados,{curso: e.value}))}} placeholder="" className="select"/>
         
         <label className="legenda">Turno:</label>
-        <Select options={options(turnos)} onChange={(e)=>{setDados(Object.assign(dados,{turno: e.value}))}} placeholder="" className="select"/>
+        <AsyncSelect defaultOptions={options('turno', dados)} onChange={(e)=>{setDados(Object.assign(dados,{turno: e.value}))}} placeholder="" className="select"/>
         
         <label className="legenda">Ano:</label>
-        <Select options={options(anos)} onChange={(e)=>{setDados(Object.assign(dados,{ano: e.value}))}} placeholder="" className="select"/>
+        <AsyncSelect defaultOptions={options('ano', dados)} onChange={(e)=>{setDados(Object.assign(dados,{ano: e.value}))}} placeholder="" className="select"/>
 
         <button type="button" className="btn btn-success">Pesquisar</button>
         <button type="button" className="btn btn-primary">Limpar Dados</button>
