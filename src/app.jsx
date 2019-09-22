b332b22ef4d315b2b1f48935e9dceed328d9bb44
felipejardim <felipejardim95@gmail.com>
2019-09-22 14:47:37 -0400

    formulario funcionando

--- src/app.jsx
+++ src/app.jsx
@@ -1,57 +1,75 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import Select from 'react-select'
 
 
 const jason = require('../src/json/cursos.json')
 
-//código para extrair as opções para o select
-let cursos = jason.map(obj=>obj.Curso)
-cursos = cursos.filter(function(elem, pos, self) {
-    return self.indexOf(elem) === pos;
-})
+window.jason = jason
 
-let campi = jason.map(obj=>obj.Campus)
-campi = campi.filter(function(elem, pos, self) {
-    return self.indexOf(elem) === pos;
-})
-
-let turnos = jason.map(obj=>obj.Turno)
-turnos = turnos.filter(function(elem, pos, self) {
-    return self.indexOf(elem) === pos;
-})
-
-let anos = jason.map(obj=>obj.Ano)
-anos = anos.filter(function(elem, pos, self) {
-    return self.indexOf(elem) === pos;
-})
-
-window.dadoss = [campi,cursos,turnos,anos]
+let options = (dados)=>{
+    let filtro = jason
 
 
-let options = function(item){
-    let arr = item.map((key)=>{return {value:key, label:key}})
-    return arr 
+    //método para filtro dos dados
+    filtro = dados.campus!==''? filtro = filtro.filter(e=>e.Campus===dados.campus):filtro;
+    filtro = dados.curso!==''? filtro = filtro.filter(e=>e.Curso===dados.curso):filtro;   
+    filtro = dados.turno!==''? filtro = filtro.filter(e=>e.Turno===dados.turno):filtro;
+    filtro = dados.ano!==''? filtro = filtro.filter(e=>e.Ano===dados.ano):filtro;
+
+    //método para troca de estrutura, passa os valores de filtro para a estrutura abaixo 
+    let algo = {campi:[],cursos:[],turnos:[],anos:[]}
+    algo.campi = filtro.map(e=>e.Campus)
+    algo.cursos = filtro.map(e=>e.Curso)
+    algo.turnos = filtro.map(e=>e.Turno)
+    algo.anos = filtro.map(e=>e.Ano)
+
+    //método para remover repetições e formatar os dados para o React-Select (um array de '{value,label}')
+    algo.campi = algo.campi.filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+    algo.cursos = algo.cursos.filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+    algo.turnos = algo.turnos.filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+    algo.anos = algo.anos.filter((elem, pos, self)=>{return self.indexOf(elem) === pos;}).map((key)=>{return{value:key, label:key}})
+
+    console.log(algo)
+
+    return algo
 }
+
+function limpacampo(pora){
+    pora.campus.current.state.value = {label:'', value:''}
+    pora.curso.current.state.value = {label:'', value:''}
+    pora.turno.current.state.value = {label:'', value:''}
+    pora.ano.current.state.value = {label:'', value:''}
+}
+
 
 export default function App(){
 
     let [dados, setDados] = useState({campus:"", curso:"", turno:"", ano:""});
+    let [lista, setLista] = useState(options(dados));
+    let [teste, setteste] = useState(1)
+    console.log(lista)
+
+    let pora ={campus: useRef(), curso: useRef(), turno: useRef(), ano: useRef()}
+    window.pora=pora
+
+
+    useEffect(()=>{setLista(options(dados));console.log('oi')}, [teste])
 
     return <>
         <label className="legenda">Campus:</label>
-        <Select options={options(campi) } onChange={(e)=>{setDados(Object.assign(dados,{campus: e.value}))}} placeholder="" className="select"/>
+        <Select ref={pora.campus} options={lista.campi} onChange={(e)=>{setDados(Object.assign(dados,{campus: e.value}));setteste(++teste)}} placeholder="" className="select"/>
         
         <label className="legenda">Curso:</label>
-        <Select options={options(cursos)} onChange={(e)=>{setDados(Object.assign(dados,{curso: e.value}))}} placeholder="" className="select"/>
+        <Select ref={pora.curso} options={lista.cursos} onChange={(e)=>{setDados(Object.assign(dados,{curso: e.value}));setteste(++teste)}} placeholder="" className="select"/>
         
         <label className="legenda">Turno:</label>
-        <Select options={options(turnos)} onChange={(e)=>{setDados(Object.assign(dados,{turno: e.value}))}} placeholder="" className="select"/>
+        <Select ref={pora.turno} options={lista.turnos} onChange={(e)=>{setDados(Object.assign(dados,{turno: e.value}));setteste(++teste)}} placeholder="" className="select"/>
         
         <label className="legenda">Ano:</label>
-        <Select options={options(anos)} onChange={(e)=>{setDados(Object.assign(dados,{ano: e.value}))}} placeholder="" className="select"/>
+        <Select ref={pora.ano}  options={lista.anos} onChange={(e)=>{setDados(Object.assign(dados,{ano: e.value}));setteste(++teste)}} placeholder="" className="select"/>
 
-        <button type="button" className="btn btn-success">Pesquisar</button>
-        <button type="button" className="btn btn-primary">Limpar Dados</button>
+        <button type="button" onClick={()=>console.log(dados)} className="btn btn-success">Pesquisar</button>
+        <button type="button" onClick={()=>{setDados({campus:'',curso:'',turno:'',ano:''}); setteste(++teste); limpacampo(pora)}} className="btn btn-primary">Limpar Dados</button>
         </>
         
     
